5cd032fb3f8ff04a878e9820fdd3bc01cecb951d
Alexander Batischev <eual.jp@gmail.com>
2017-06-12 22:58:14 +0300

    Add git2 dependency

--- shell.nix
+++ shell.nix
@@ -6,9 +6,15 @@ in rec {
     name = "git-submerge-env";
     version = "0.0.1";
     src = ./.;
-    buildInputs = with pkgs.rustUnstable; [
-      rustc
-      cargo
+    buildInputs = [
+      pkgs.rustUnstable.rustc
+      pkgs.rustUnstable.cargo
+
+      pkgs.pkgconfig
+      pkgs.openssl
+      pkgs.libssh2
+      pkgs.libgit2
+      pkgs.cmake
     ];
   };
 }
